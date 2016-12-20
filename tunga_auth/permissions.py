a0b24ec0327f5ea4f15ea56f000293cd07aad1ef
david <tdsemakula@gmail.com>
2016-12-20 06:25:23 +0300

    Fix: allow authenticated non-admins to retrieve user details

--- tunga_auth/permissions.py
+++ tunga_auth/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-from rest_framework.permissions import IsAdminUser, SAFE_METHODS
+from rest_framework.permissions import IsAdminUser, SAFE_METHODS, IsAuthenticated
 
 from tunga_auth.utils import get_session_visitor_email
 
@@ -8,7 +8,7 @@ class IsAuthenticatedOrEmailVisitorReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS and get_session_visitor_email(request):
             return True
-        return IsAdminUser().has_permission(request, view)
+        return IsAuthenticated().has_permission(request, view)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
