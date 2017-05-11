11a97f72436db494ea564c96231120318cb858b9
david <tdsemakula@gmail.com>
2017-05-11 09:10:15 +0300

    Only one admin email for task creation

--- tunga_tasks/notifications.py
+++ tunga_tasks/notifications.py
@@ -150,7 +150,9 @@ def notify_new_task_client_receipt_email(instance, new_user=False, reminder=Fals
 @job
 def notify_new_task_admin(instance, new_user=False, completed=False, call_scheduled=False):
     notify_new_task_admin_slack(instance, new_user=new_user, completed=completed, call_scheduled=call_scheduled)
-    notify_new_task_admin_email(instance, new_user=new_user, completed=completed, call_scheduled=call_scheduled)
+    if not (completed or call_scheduled):
+        # Only initial task creation will be reported via email
+        notify_new_task_admin_email(instance, new_user=new_user, completed=completed, call_scheduled=call_scheduled)
 
 
 @job
