00c8d70cd23d42cc897d4959e6faafd3bcf3d744
david <tdsemakula@gmail.com>
2017-05-27 09:06:59 +0300

    Enable basic auth

--- tunga/settings.py
+++ tunga/settings.py
@@ -288,6 +288,7 @@ REST_FRAMEWORK = {
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
