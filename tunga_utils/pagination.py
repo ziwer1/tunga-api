2fa7973679556ddbbbc89262f8cd940eefecdf5b
david <tdsemakula@gmail.com>
2017-06-12 18:43:27 +0300

    Increase page size limit for pagination classes

--- tunga_utils/pagination.py
+++ tunga_utils/pagination.py
@@ -4,11 +4,10 @@ from rest_framework.pagination import PageNumberPagination
 class DefaultPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
-    max_page_size = 100
+    max_page_size = 10000
 
 
 class LargeResultsSetPagination(PageNumberPagination):
     page_size = 50
     page_size_query_param = 'page_size'
-    max_page_size = 1000
-
+    max_page_size = 10000
